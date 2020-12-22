9d1263eaf9b9e7dd7bb4997d2ef4ee08e00e1457
Hystersis <71912794+Hystersis@users.noreply.github.com>
2020-12-23 08:58:09 +1100

    Graphics v0.1.2

--- sshi_graphics.py
+++ sshi_graphics.py
@@ -1,11 +1,26 @@
 # Graphics for Game
-
+import random
 import pygame
+import astar as at
 
-look_up_table = []
 
-def init():
+global maze
+maze = []
+b = []
+for x in range(256):
+    for y in range(256):
+        b.append(0)
+    maze.append(b)
+    b = []
 
-def image(num):
+class mv_prtcl(pygame.sprite.Sprite):
+    def __init__(self,strt,end,imge):
+        self.path = at.main(maze,strt,end)
+        self.image = imge
+        self.rect = self.image.get_rect()
+        self.rect.topleft = strt
+    def update(self):
+        pass
 
-def get_cor(num):
+# def main():
+#     screen =
