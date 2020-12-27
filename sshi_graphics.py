a1522a0d72aed7d6bff58ee5c9a3c783bc36396b
Hystersis <71912794+Hystersis@users.noreply.github.com>
2020-12-27 15:04:15 +1100

    Graphics v0.1.3

--- sshi_graphics.py
+++ sshi_graphics.py
@@ -1,8 +1,8 @@
 # Graphics for Game
 import random
 import pygame
-import astar as at
-
+import sshi_InDrw as ld
+import numpy as np
 
 global maze
 maze = []
@@ -15,12 +15,22 @@ for x in range(256):
 
 class mv_prtcl(pygame.sprite.Sprite):
     def __init__(self,strt,end,imge):
-        self.path = at.main(maze,strt,end)
-        self.image = imge
+        self.path = ld.draw_line(np.zeros((256,256)),strt[0],strt[1],end[0],end[1])
+        self.image = pygame.image.load(imge)
         self.rect = self.image.get_rect()
         self.rect.topleft = strt
+        print(self.path)
     def update(self):
-        pass
+        if len(self.path) <= 1:
+            # Add any animations for end here
+            self.kill()
+        else:
+            self.rect.topleft = self.path.pop(0)
 
-# def main():
-#     screen =
+pr = mv_prtcl([0,0],[256,256],"dodger_1.png")
+ddger_group = pygame.sprite.Group()
+ddger_group.add(pr)
+
+while len(ddger_group) > 0:
+    ddger_group.update()
+    print("ran")
