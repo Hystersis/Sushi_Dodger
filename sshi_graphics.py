85ffe3db992b40261dfe28be9b613ba5dec61dd1
Hystersis <71912794+Hystersis@users.noreply.github.com>
2021-02-06 09:06:44 +1100

    Graphics v0.1.5.4 Main v0.1.2.9

--- sshi_graphics.py
+++ sshi_graphics.py
@@ -4,10 +4,16 @@ import pygame
 import sshi_LnDrw as ld
 import numpy as np
 
-global maze, particles
+global maze, particles, litr
 
+pygame.freetype.init(resolution=48)
 
+litr = {'Active' : None,
+        'Paused' : None,
+        'Died' : "defeat_screen.png",
+        'Won' : None} # Code must be added here in the future to list the .png for 'Won'
 particles = pygame.sprite.Group()
+
 class mv_prtcl(pygame.sprite.Sprite):
     def __init__(self,strt,end,imge):
         self.path = ld.draw_line(np.zeros((256,256)),strt[0],strt[1],end[0],end[1])
@@ -22,21 +28,56 @@ class mv_prtcl(pygame.sprite.Sprite):
         else:
             self.rect.topleft = self.path.pop(0)
 
-def screenLow(screen):
-    screen = screen.copy()
-    return screen
-
-def screenHigh(screen):
-    screen = screen.copy()
-    particles.update()
-    particles.draw(screen)
-    return screen
 
 def particle(mode,image,map = None,startC = None, endC = None,pr = None,inplace = False):
     pr = mv_prtcl(startC,endC,image) if mode == 0 else pr
     # pr = /Add next particle/
     particles.add(pr)
 
-def scaling(screen,screen_width = None,screen_height = None):
-    ns = pygame.transform.scale(screen, (screen_width, screen_height))
-    return ns
+class transition:
+    """tr is transition"""
+    count = 0
+    def __init__(self):
+        self.image = None
+        transition.count += 1
+        if transition.count > 1:
+            raise ValueError('The amount of transition is over threshold.')
+    def update(self,image):
+        if image != None:
+            self.image = pygame.image.load(image)
+            self.rect = self.image.get_rect()
+    def draw(self):
+        return self.image
+
+def screenLow(screen):
+    screen = screen.copy()
+    return screen
+
+def screenHigh(screen,gm):
+    screen = screen.copy()
+    particles.update()
+    particles.draw(screen)
+    tr.update(litr[gm])
+    screen.blit(tr.draw(),[42,0]) if tr.draw() != None else None
+    return screen
+
+tr = transition()
+
+def word_wrap(surf, text, font, color=(0, 0, 0)):
+    font.origin = True
+    words = text.split(' ')
+    width, height = surf.get_size()
+    line_spacing = font.get_sized_height() + 2
+    x, y = 0, line_spacing
+    space = font.get_rect(' ')
+    for word in words:
+        bounds = font.get_rect(word)
+        if x + bounds.width + bounds.x >= width:
+            x, y = 0, y + line_spacing
+        if x + bounds.width + bounds.x >= width:
+            raise ValueError("word too wide for the surface")
+        if y + bounds.height - bounds.y >= height:
+            raise ValueError("text to long for the surface")
+        font.render_to(surf, (x, y), None, color)
+        x += bounds.width + space.width
+    return x, y
