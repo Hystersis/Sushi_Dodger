6a035d884a5007a6a1d18a473b185d4caabb8177
Hystersis <71912794+Hystersis@users.noreply.github.com>
2021-06-12 09:00:39 +1000

    Added missile and better die screen
    *Added a missile that tracks ddger and orientates towards it
    This missile is going to be imporved later with better physics
    
    *The die screen has animations now

---
+++ sshi_special.py
@@ -0,0 +1,48 @@
+import pygame
+import os
+from operator import sub, add
+
+
+class missile(pygame.sprite.Sprite):
+    def __init__(self, start_pos, orientation: int):
+        super().__init__()
+        self.orientation = orientation
+        self.orientate()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = start_pos
+
+    def update(self, ddger):
+        self.delta = list(map(sign,
+                              list(map(sub, ddger.rect.center,
+                                       self.rect.center))))
+        print(self.delta,'\t',end='')
+        self.rect.topleft = tuple(map(add, self.delta, self.rect.topleft))
+        if 0 in self.delta:
+            self.orientation = (lambda x, y: (0 if x == abs(x) else 4)
+                                + (2 if y == abs(y) else 6))(*self.delta)
+        else:
+            self.orientation = (lambda x, y: abs((-6 if x != abs(x) else 2)
+                                - y))(*self.delta)
+        print(self.orientation)
+        self.orientate()
+
+    def orientate(self):
+        '''Orientates the missile to be faces the 'right' direction
+        in 90° incraments'''
+        # Orintation is 0 for facing bottom
+        # Orientation is 1 for facing left-bottom (45o)
+        if self.orientation % 2 == 0:
+            self.image = pygame.image.load(os.path.join('Assets', 'missile.png'))
+        else:
+            # Rotated sprite
+            self.image = pygame.image.load(os.path.join('Assets', 'missile_rot.png'))
+        self.image = pygame.transform.rotate(self.image, self.orientation // 2 * 90)
+
+
+def sign(num: int) -> int:
+    if num > 0:
+        return 1
+    elif num == 0:
+        return 0
+    else:
+        return -1
--- test2.py
+++ test2.py
@@ -1,14 +1,10 @@
-def closer(a, b, c, by=1):
-    '''This function returns what the effect of by will be on +/- it
-    to b, this returns which operation should be performed to get the closetest
-    to a or c'''
-    smaller = min(a, c)
-    larger = max(a, c)
-    minus = [(b - by) - smaller, b - by]
-    addition = [larger - (b + by), b + by]
-    if min(minus[0], addition[0]) == minus[0]:
-        return minus[1]
-    else:
-        return addition[1]
+import pygame
+import os
 
-print(closer(10, 16, 20, 1))
+a = pygame.image.load(os.path.join('Assets','mw_sushi_dodger_sprites.png'))
+
+b = pygame.image.tostring(a, 'RGBA')
+print(b)
+print('\n===8 bit===\n')
+c = pygame.image.tostring(a, 'P')
+print(c)
