ba598397f3e8a8a53ac6d2f47e87e33aa5b1b449
Hystersis <71912794+Hystersis@users.noreply.github.com>
2021-07-31 11:46:55 +1000

    Better laser enemy
    *It can move to were it can hit the ddger

--- sshi_special.py
+++ sshi_special.py
@@ -1,3 +1,4 @@
+from sshi_core import minmax
 import pygame
 import os
 from operator import sub, add, mul
@@ -26,6 +27,7 @@ class missile(pygame.sprite.Sprite):
         super().__init__()
         self.colours = [(174, 35, 52), (232, 59, 59), (249, 194, 43)]
         self.time = time.time()
+        self.life_span = 7
         self.orientation = orientation
         self.orientate(self.orientation)
         self.image = pygame.Surface((256, 256), flags=pygame.SRCALPHA)
@@ -37,7 +39,7 @@ class missile(pygame.sprite.Sprite):
         self.dirnx = 0
         self.dirny = 0
         self.particles = []
-        self.life_span = 1
+        self.explosion = []
         self._hit = True
         self.last_dist = math.inf
 
@@ -48,10 +50,12 @@ class missile(pygame.sprite.Sprite):
 
         # This returns the backside coords of the craft
         self.backside = list(map(add, self.rect2.center,
-                                  tuple(map(sub, (0, 0), map(sign, delta)))
-                                  * (self.rect2.size[0] // 2)))
+                                 tuple(map(sub, (0, 0), map(sign, delta)))
+                                 * (self.rect2.size[0] // 2)))
 
-        self.particles.append([self.backside, [random.gauss(0, 0.6), sign(self.rect2.center[1] - self.backside[1]) * 2], random.uniform(3, 4)])
+        if self._hit:
+            self.particles.append([self.backside, [random.gauss(0, 0.6), sign(self.rect2.center[1] - self.backside[1]) * 2], random.uniform(3, 4)])
+
         for p in self.particles:
             p[0][0] += p[1][0]
             p[0][1] += p[1][1]
@@ -59,9 +63,24 @@ class missile(pygame.sprite.Sprite):
             colour = [a + b + c for a, b, c in zip(map(mul, self.colours[0], repeat(sinsq(min(p[2], 2), 4, 2))),
                         map(mul, self.colours[1], repeat(sinsq(p[2], 4))),
                         map(mul, self.colours[2], repeat(sinsq(max(p[2], 2), 4, 2))))]
-            if self._hit:
-                pygame.draw.circle(self.image, colour, tuple(map(int, p[0])), int(p[2]))
+            pygame.draw.circle(self.image, colour, tuple(map(int, p[0])), int(p[2]))
+
+        for p in self.explosion:
+            p[1] -= 1.5
+            for i in [[0, 1], [0, -1], [1, 0], [1, 1], [1, -1], [-1, 0], [-1, 1], [-1, -1]]:
+                self.particles.append([list(p[0]), i, random.uniform(3, 4)])
+            expface = pygame.Surface((32, 32), flags=pygame.SRCALPHA)
+            # Black outer circle
+            pygame.draw.circle(expface, (46, 34, 47), (16, 16), 16 - p[1])
+            # Red inner circle
+            pygame.draw.circle(expface, (179, 56, 49), (16, 16), 16 - p[1] - 2)
+            # Transparent cut-out circle
+            pygame.draw.circle(expface, (0, 0, 0, 0), (16, 16),
+                               12 - p[1] - int(p[1] / 3.2))
+            self.image.blit(expface, (p[0][0] - 16, p[0][1] - 16))
+
         self.particles = [p for p in self.particles if p[2] > 0]
+        self.explosion = [p for p in self.explosion if p[1] > 0]
 
         if self._hit:
             d = math.sqrt(delta[0] ** 2 + delta[1] ** 2)  # Pythagoras for distance
@@ -76,18 +95,13 @@ class missile(pygame.sprite.Sprite):
             self.dirnx += accel[0]
             self.dirny += accel[1]
 
-            NSdelta = sign(accel[1]) + 2
-
-            if sign(accel[0]) == -1:
-                NSdelta = abs(NSdelta - 8)
-            elif sign(accel[0]) == 0:
-                NSdelta = (NSdelta - 1) * 2
-            self.orientate(NSdelta)
+            ordegrees = anglebetween(self.rect2.topleft, tuple(map(add, self.rect2.topleft, (self.dirnx, self.dirny))))
+            self.orientate(abs((ordegrees + 90) // 45))
 
             self.rect2.topleft = tuple(map(minmax, (0, 0), tuple(map(add, (self.dirnx, self.dirny), self.rect2.topleft)), (239, 239)))
             self.image.blit(self.missile, self.rect2.topleft)
 
-            if esttime <= 3 and esttime >= self.last_dist or (time.time() - self.time) > 7:
+            if esttime <= 3 and esttime >= self.last_dist or (time.time() - self.time) > self.life_span:
                 self.do_hit(ddger, sshiG)
                 self._hit = False
             self.last_dist = esttime
@@ -102,13 +116,14 @@ class missile(pygame.sprite.Sprite):
             Orientates the missile in 45o increments - 0 is facing bottom left
                  up
             +---+---+---+
-            | 5 | 4 | 3 |
+            | 5 | 4 | 3 |       1
             +---+---+---+
-       left | 6 |N/A| 2 | right
+       left | 6 |N/A| 2 | right 0
             +---+---+---+
-            | 7 | 0 | 1 |
-            +---+---+---+
-                down
+            | 7 | 0 | 1 |       -1
+            +---+---+---+        ^
+                down             y
+              -1  0   1        <x
         """
         # Orintation is 0 for facing bottom
         # Orientation is 1 for facing left-bottom (45o)
@@ -120,12 +135,27 @@ class missile(pygame.sprite.Sprite):
             self.missile = pygame.image.load(os.path.join('Assets', 'missile_rot.png'))
         durview = pygame.Surface((16, 16))
         durview.fill(colour)
-        print(durview)
-        p16 = int((7 - (time.time() - self.time)) / 0.4375)
+        p16 = int((self.life_span - (time.time() - self.time)) / 0.4375)
         self.missile = pygame.transform.rotate(self.missile, orientation // 2 * 90)
         self.missile.blit(durview, (0, p16), special_flags=pygame.BLEND_RGBA_MULT)
 
     def do_hit(self, ddger, sshiG):
+        """Calculates hit when the missile explodes
+
+        Parameters
+        ----------
+        ddger : pygame.sprite.Sprite
+            The ddger, so it can calculate hits
+        sshiG : pygame.sprite.Group
+            sshi group, for hits
+
+        Notes
+        -----
+        The algorithm creates a circle of radius 30 then sees if that circle centered
+        on the missile collides with the sprite mask of the sprites of either
+        ddger or sshi_group, the sprite mask makes sure in only collides with
+        the sprite not the empty space.
+        """
         if self._hit:
             hit_surface = pygame.Surface((256, 256), flags=pygame.SRCALPHA)
             pygame.draw.circle(hit_surface, (0, 0, 0), self.rect2.center, 30)
@@ -139,20 +169,20 @@ class missile(pygame.sprite.Sprite):
                             ddger.rect.topleft)
             if type(hit) is tuple:
                 ddger.killed()
-            self.particles.append([list(self.rect2.center), [0, 0], 20])
+            self.explosion.append([self.rect2.center, 16])
 
     def calspeed(self, delta: tuple, sxy: tuple, dxy: tuple, dirn: tuple):
         """Calculates the speed that the ddger is travelling at relative to the missile
 
         Parameters
         ----------
-        delta : tuple | list
+        delta : tuple or list
             This is the delta calculated earlier
-        sxy : tuple | list
+        sxy : tuple or list
             self xy - the x or y coordinate of the missile
-        dxy : tuple | list
+        dxy : tuple or list
             ddger xy - the x or y coordinate of the ddger
-        dirn : tuple | list
+        dirn : tuple or list
             ddger's dirn(x/y) - its movement in different direction
 
         Returns
@@ -173,7 +203,7 @@ class missile(pygame.sprite.Sprite):
 
         Returns
         -------
-        float | int
+        float or int
             A value that gets smaller the closer to zero and it cannot be
             bigger than 1
         """
@@ -189,13 +219,77 @@ class laser_enemy(pygame.sprite.Sprite):
         self.rect.topleft = (0, 0)
         self.rect2 = self.ship.get_rect()
         self.charge_cycle = cycle(range(18))
+        self.time = time.time()
 
     def update(self, ddger, sshiG: pygame.sprite.Group):
+        """Movement and firing solutions for laser
+
+        Parameters
+        ----------
+        ddger : pygame.sprite.Sprite
+            The dodger/player
+        sshiG : pygame.sprite.Group
+            The group of sshi
+
+        Notes
+        -----
+        The algorithm 'draws' a line of where the ddger will be
+        if it continued on its current directory, it then sees
+        where the dodger will be at the time it would be able
+        fire laser, if it was fired now - it then checks if it can hit
+        the ddger by drawing lines off it. If the missile cannot hit it
+        it calculates what postion it should be to hit it by - but
+        every position moved is more delay for the laser so the coordinate
+        that it predicts the ddger will be at it is moved by that amount and
+        then it checks if it can hit.
+        The laser is three pixels wide.
+        """
+        self.image.fill((0, 0, 0, 0))
+        # Calculate where to move
+        ang = abs(anglebetween(ddger.rect.center, self.rect2.center))
+        if ang % 45 == 0:
+            self.fire()
+        else:
+            # https://stackoverflow.com/questions/12141150/from-list-of-integers-get-number-closest-to-a-given-value
+            # This returns the angle closest to the current angle
+            angto = min([ang // 45 * 45, (ang // 45 + 1) * 45], key=lambda x:abs(x-ang))
+            # This makes the underflow/overflow of an angle
+            trangto = (angto - 90) % 360
+            delta = math.sqrt((ddger.rect.center[0] - self.rect2.topleft[0]) ** 2 +
+                              (ddger.rect.center[1] - self.rect2.topleft[1]) ** 2)
+            delta2 = (ddger.rect.center[0] - self.rect2.topleft[0],
+                      ddger.rect.center[1] - self.rect2.topleft[1])
+            if delta > 100:
+                # Move towards dodger
+                moveto = tuple(map(add, self.rect2.center, tuple(map(sign, delta2))))
+            elif delta < 30:
+                # Back away from dodger
+                moveto = tuple(map(add, self.rect2.center, tuple(map(sign, delta2, (-1, -1)))))
+            else:
+                moveto = (self.rect2.center[0]
+                                    + math.cos(trangto * math.pi / 180),
+                                    self.rect2.center[1]
+                                    + math.sin(trangto * math.pi / 180))
+            self.rect2.center = tuple(map(minmax, (8, 8), moveto, (247, 247)))
+        ordegrees = anglebetween(self.rect2.center, ddger.rect.center)
+        self.orientate(abs((ordegrees - 90) // 45))
+        self.image.blit(self.ship, self.rect2.topleft)
+        # for v in range(0, 256):
+        #     gopm = [self.rect2.center]
+        #     est_pos = dco[min(len(dco), 13 + v)]
+        #     collide_offset = anglebetween(self.rect2.center, est_pos) - 90
+        #     if collide_offset % 45 == 0:
+        #         go_to_pos = collide_offset
+        #     if go_to_pos == self.rect2.center:
+        #         self.fire()
+        #     self.rect2.center += tuple(map(sign, map(sub, go_to_pos, self.rect2.center)))
+
+        delta = tuple(map(sub, self.rect2.topleft, ddger.rect.topleft))
         Sdelta = tuple(map(sign, delta))
         # This returns the backside coords of the craft
-        self.backside = tuple(map(add, self.rect.center,
-                                  tuple(map(sub, (0, 0), Sdelta))
-                                  * (self.rect.size[0] // 2)))
+        self.backside = list(map(add, self.rect2.center,
+                                 tuple(map(sub, (0, 0), map(sign, delta)))
+                                 * (self.rect2.size[0] // 2)))
         # Mapping of cycle:
         # 0 - reset everything
         # 1-10 charge laser
@@ -205,20 +299,52 @@ class laser_enemy(pygame.sprite.Sprite):
         #    13 calculate hits
         #    14 leave remanent of laser blast
         # 15-17 cooldown
-        n = next(self.charge_cycle)
-        if n >= 1 and n < 11:
-            # -----Charge of laser-----
-            pygame.draw.circle(self.image, tuple(map(add,
-                                                     map(mul,
-                                                         (255, 255, 255, 150),
-                                                         repeat(1 / 10 * (11 - n))),
-                                                      map(mul,
-                                                          (77, 155, 230, 245),
-                                                          repeat(1 / 10 * n)))), 
-                               self.backside)
-        elif n >= 11 and n < 15:
-            # -----Firing of laser-----
-            pygame.draw.line()
+        # n = next(self.charge_cycle)
+        # if n >= 1 and n < 11:
+        #     # -----Charge of laser-----
+        #     pygame.draw.circle(self.image, tuple(map(add,
+        #                                              map(mul,
+        #                                                  (255, 255, 255, 150),
+        #                                                  repeat(1 / 10 * (11 - n))),
+        #                                               map(mul,
+        #                                                   (77, 155, 230, 245),
+        #                                                   repeat(1 / 10 * n)))), 
+        #                        self.backside)
+        # elif n >= 11 and n < 15:
+        #     # -----Firing of laser-----
+        #     pygame.draw.line()
+
+    def fire(self):
+        """The craft fires in one directions, as it won't fire often
+        """
+        print('fire!', time.time())
+
+    def orientate(self, orientation):
+        """Orientates the laser in one of eight orientates as listed below
+
+        Parameters
+        ----------
+        orientation : int
+            Orientates the missile in 45o increments - 0 is facing bottom left
+                 up
+            +---+---+---+
+            | 5 | 4 | 3 |       1
+            +---+---+---+
+       left | 6 |N/A| 2 | right 0
+            +---+---+---+
+            | 7 | 0 | 1 |       -1
+            +---+---+---+        ^
+                down             y
+              -1  0   1        <x
+        """
+        # Orintation is 0 for facing bottom
+        # Orientation is 1 for facing left-bottom (45o)
+        if orientation % 2 == 0:
+            self.ship = pygame.image.load(os.path.join('Assets', 'ship.png'))
+        else:
+            # Rotated sprite
+            self.ship = pygame.image.load(os.path.join('Assets', 'ship_rot.png'))
+        self.ship = pygame.transform.rotate(self.ship, (orientation // 2 * 90))
 
 
 # From https://realpython.com/python-rounding/#rounding-down
@@ -240,3 +366,63 @@ def minmax(a, b, c):
     '''This function returns the middle variable between the min variable
     and the max variable.'''
     return (lambda x: sorted(x)[1])([a, b, c])
+
+
+def anglebetween(avector: tuple, bvector: tuple):
+    """Find the angle between two vectors using atan2
+
+    Parameters
+    ----------
+    avector : tuple
+        The first 2D vector
+    bvector : tuple
+        The second 2D vector
+
+    Notes
+    -----
+    The algorithm first finds the deltas: ydelta is bvector.y - avector.y then
+    xdelta is bvector.x - avector.x, it than does atan2(ydelta, xdelta),
+    it then converts that to degrees
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    return math.degrees(math.atan2(bvector[1] - avector[1],
+                                   bvector[0] - avector[0]))
+
+
+def closer(a, b, c, by=1, maximise=False):
+    """This angle determines which number b is closer too
+    either a or c
+
+    Parameters
+    ----------
+    a : int or float
+        1st number to test which is closer to
+    b : int or float
+        The number to test how close it is to others
+    c : int or float
+        2nd number to test which is closer to
+    by : int, optional
+        The movement of b so how much should b be moved by, by default 1
+    maximise : bool, optional
+        Should invert the result instead returning which one b is further from,
+        by default False
+
+    Returns
+    -------
+    int or float
+        Returns what the b value should be so it is closer to either of the
+        two values (a or c)
+    """
+    smaller = min(a, c)
+    larger = max(a, c)
+    minus = [(b - by) - smaller, b - by]
+    addition = [larger - (b + by), b + by]
+    if min(minus[0], addition[0]) == minus[0]\
+            or maximise and min(minus[0], addition[0]) == addition[0]:
+        return minus[1]
+    else:
+        return addition[1]
